c2df9a33f8a7f9153c0f546526fc924e551d1bb6
Leandro de Lara Celio <122060742+Ledelara@users.noreply.github.com>
2025-01-28 11:24:15 -0300

    ✨ feat: interface ILoginUserResponse

--- src/types/@types.ts
+++ src/types/@types.ts
@@ -9,3 +9,13 @@ export interface ILoginUserPayload {
     email: string;
     password: string;
 }
+
+export interface ILoginUserResponse {
+    message: string;
+    result: {
+        token: string;
+        email: string;
+        name: string;
+        userId: string;
+    };
+}
