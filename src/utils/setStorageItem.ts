bd3cae96fff388ad87dcfca44def15a15ed5f281
Leandro de Lara Celio <122060742+Ledelara@users.noreply.github.com>
2025-01-28 11:25:07 -0300

    🐛 fix: armazenando token jwt

--- src/utils/setStorageItem.ts
+++ src/utils/setStorageItem.ts
@@ -1,5 +1,8 @@
-export const setSotrageItem = (key: string, value: string) => {
-    if (typeof window === 'undefined') {
+export const setStorageItem = (key: string, value: string) => {
+    if (typeof window !== 'undefined') {
+        console.log('Salvando token no localStorage');
         localStorage.setItem(key, value);
+    } else {
+        console.log('Não é possível acessar localStorage no servidor');
     }
 };
