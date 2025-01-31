c033a0c899ae22f6fd3783a555a9bba7faff120a
Leandro de Lara Celio <122060742+Ledelara@users.noreply.github.com>
2025-01-30 22:58:27 -0300

    🐛 fix: corrigindo chamada do QueryProvider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 'use client'
 import PrivateRoute from "@/components/PrivateRoute";
 import { checkIsPublicRoute } from "@/functions/check-is-public-route";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { usePathname } from "next/navigation";
 
 export default function RootLayout({
@@ -12,15 +13,19 @@ export default function RootLayout({
   const pathname = usePathname();
   const isPublicPage = checkIsPublicRoute(pathname!);
 
+  const queryClient = new QueryClient();
+
   return (
     <html lang="pt-BR">
       <body>
-        {isPublicPage && children}
-        {!isPublicPage && (
-          <PrivateRoute>
-            {children}
-          </PrivateRoute>
-        )}
+        <QueryClientProvider client={queryClient}>
+          {isPublicPage && children}
+          {!isPublicPage && (
+            <PrivateRoute>
+              {children}
+            </PrivateRoute>
+          )}
+        </QueryClientProvider>
       </body>
     </html>
   );
