ced1eef41a21f639755d318fbf3622ae8aacc0f6
Leandro de Lara Celio <122060742+Ledelara@users.noreply.github.com>
2025-01-31 19:19:57 -0300

    🚧 feat: dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,28 +1,92 @@
 'use client'
 import { useUserQuery } from "@/services/queries";
-import styles from "./page.module.css";
-import ThemeButton from "@/components/Buttons/ThemeButton/ThemeButton";
+import { Box, Card, Container, Typography } from "@mui/material";
+import LoadingComponent from "@/components/Loading/Loading";
 
 export default function Dashboard() {
   const userId = typeof window !== "undefined" ? localStorage.getItem("userId") : null;
-  const { data: user, isLoading, isError, error } = useUserQuery(userId);
-
-  if (isLoading) return <p>Carregando...</p>;
-  if (isError) return <p>Erro ao carregar usuário: {error?.message}</p>;
+  const { data: user, isLoading, isError } = useUserQuery(userId);
 
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <h1>Você está logado!</h1>
+    <Container
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+        <LoadingComponent isLoading={isLoading} />  
+        {isError && <Typography variant="h6" component="p">Erro ao carregar usuári.</Typography>}
         {user && (
-          <div>
-            <p>Bem-vindo, {user.name}!</p>
-            <p>Conta: {user.accountNumber}</p>
-            <p>Saldo: {user.balance}</p>
-            <ThemeButton />
-          </div>
+          <Card
+            sx={{
+              padding: 2,
+              width: 500,
+              height: 200,
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+            }}
+          >
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+              }}
+            >
+              <Typography
+                variant="h5"
+                component="h1"
+                sx={{
+                  textAlign: "left",
+                }}
+              >
+                Bem vindo, {user?.name}!
+              </Typography>
+              <Typography
+                variant="body1"
+                component="p"
+                sx={{
+                  textAlign: "left"
+                }}
+              >
+                Conta: {user?.accountNumber}
+              </Typography>
+            </Box>
+
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+              }}
+            >
+              <Typography
+                variant="h5"
+                component="h1"
+                sx={{
+                  textAlign: "right",
+                }}
+              >
+                Saldo
+              </Typography>
+              <Typography
+                variant="body1"
+                component="p"
+                sx={{
+                  color: (user?.balance ?? 0) == 0 ? "error.main" : "success.main",
+                  fontWeight: "bold",
+                  fontSize: "1.5rem",
+                  textAlign: "right"
+                }}
+              >
+                R$ {user?.balance}
+              </Typography>
+            </Box>
+          </Card>
         )}
-      </main>
-    </div>
+    </Container>
   );
 }
