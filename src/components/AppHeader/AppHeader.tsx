386e743f6d696ebe97eb6c9fe720a1d82208de17
Leandro de Lara Celio <122060742+Ledelara@users.noreply.github.com>
2025-02-02 17:42:51 -0300

    ✨ feat: aplicando função de logout

--- src/components/AppHeader/AppHeader.tsx
+++ src/components/AppHeader/AppHeader.tsx
@@ -1,7 +1,27 @@
 import { AppBar, Box, Button, Toolbar } from "@mui/material";
 import ThemeButton from "../Buttons/ThemeButton/ThemeButton";
+import ModalComponent from "../Modal/ModalComponent";
+import { useState } from "react";
+import { removeStorageItem } from "@/utils/removeStorageItem";
+import { useRouter } from "next/navigation";
+import { checkUserAuthenticated } from "@/functions/check-user-authenticated";
 
 export default function AppHeader() {
+    const [isOpen, setIsOpen] = useState(false);
+    const { push } = useRouter();
+
+    const authenticared = checkUserAuthenticated();
+
+    const logout = () => {
+        removeStorageItem("userToken");
+        removeStorageItem("userId");
+
+        if (removeStorageItem("userToken") !== null && removeStorageItem("userId") !== null) {
+            setIsOpen(false);
+            push("/login");
+        };
+    };
+
     return (
         <Box sx={{ flexGrow: 1 }}>
             <AppBar position="static">
@@ -12,7 +32,14 @@ export default function AppHeader() {
                     }}
                 >
                     <ThemeButton />
-                    <Button color="inherit">Sair</Button>
+                    {authenticared && <Button color="inherit" onClick={() => setIsOpen(true)}>Sair</Button>}
+                    <ModalComponent
+                        isOpen={isOpen}
+                        onClose={() => setIsOpen(false)}
+                        onConfirm={logout}
+                        modalTitle="Sair"
+                        modalDescription="Tem certeza de que deseja sair?"
+                    />
                 </Toolbar>
             </AppBar>
         </Box>
