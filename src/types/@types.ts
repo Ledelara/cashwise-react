3c4cb07438ed2499354729648076605c9e202f83
Leandro de Lara Celio <122060742+Ledelara@users.noreply.github.com>
2025-02-07 09:09:52 -0300

    ♻️ refactor: removendo interface IDepositPayload

--- src/types/@types.ts
+++ src/types/@types.ts
@@ -20,10 +20,6 @@ export interface ILoginUserResponse {
     };
 }
 
-export interface IDepositPayload {
-    amount: number;
-}
-
 export interface IUser {
     name: string,
     accountNumber?: string,
