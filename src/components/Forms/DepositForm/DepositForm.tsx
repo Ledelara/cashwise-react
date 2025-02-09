8533216badc15bcf71b0bd1064b48a8287da9688
Leandro de Lara Celio <122060742+Ledelara@users.noreply.github.com>
2025-02-09 18:19:02 -0300

    ♻️ refactor: envolvendo DepositForm em um Modal

--- src/components/Forms/DepositForm/DepositForm.tsx
+++ src/components/Forms/DepositForm/DepositForm.tsx
@@ -1,11 +1,17 @@
 import { depositSchema } from "@/schemas/depositSchema";
 import { useDeposit } from "@/services/mutate";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { TextField, useTheme } from "@mui/material";
+import { Box, Modal, TextField, useTheme } from "@mui/material";
 import { useForm } from "react-hook-form";
 import Form from "../Form";
 
-export default function DepositForm() {
+interface DepositFormProps {
+    isOpen: boolean;
+    onClose: () => void;
+    modalTitle: string;
+}
+
+export default function DepositForm({ isOpen, onClose, modalTitle }: DepositFormProps) {
     const { palette } = useTheme();
 
     const { register, handleSubmit, formState: { errors } } = useForm<{ amount: number }>({
@@ -19,44 +25,64 @@ export default function DepositForm() {
         const formattedAmount = parseFloat(String(data.amount).replace(",", "."));
         createDepositMutation.mutate({ amount: formattedAmount, id: getUserId ?? '' });
     };
-    
+
 
     return (
-        <div>
-            <Form
-                onSubmit={handleSubmit(onSubmit)}
-                style={{
-                    backgroundColor: palette.background.paper,
-                    padding: '20px',
-                    borderRadius: '10px',
-                    boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)',
+        <Modal
+            open={isOpen}
+            onClose={onClose}
+            aria-labelledby={modalTitle}
+        >
+            <Box
+                sx={{
+                    position: 'absolute',
+                    top: '50%',
+                    left: '50%',
+                    transform: 'translate(-50%, -50%)',
+                    width: 300,
+                    bgcolor: 'background.paper',
+                    borderRadius: '5px',
+                    boxShadow: 24,
                     display: 'flex',
                     flexDirection: 'column',
-                    justifyContent: 'center',
                     alignItems: 'center',
-                    gap: '10px',
-                    width: '300px',
+                    justifyContent: 'center',
+                    padding: '10px',
                 }}
-                buttonLabel={loading ? 'Carregando...' : 'Depositar'}
-                loading={loading}
             >
-                <TextField
-                    id="amount"
-                    label="Valor"
-                    variant="outlined"
-                    type="text"
-                    {...register("amount", { valueAsNumber: true })}
-                    error={!!errors?.amount}
-                    helperText={errors?.amount?.message}
+                <Form
+                    onSubmit={handleSubmit(onSubmit)}
                     style={{
-                        width: "100%",
-                        backgroundColor: palette.background.default,
+                        backgroundColor: palette.background.paper,
+                        padding: '20px',
+                        borderRadius: '10px',
+                        boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)',
+                        display: 'flex',
+                        flexDirection: 'column',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        gap: '10px',
+                        width: '300px',
                     }}
-                />
-
-            </Form>
-
-        </div>
+                    buttonLabel={loading ? 'Carregando...' : 'Depositar'}
+                    loading={loading}
+                >
+                    <TextField
+                        id="amount"
+                        label="Valor"
+                        variant="outlined"
+                        type="text"
+                        {...register("amount", { valueAsNumber: true })}
+                        error={!!errors?.amount}
+                        helperText={errors?.amount?.message}
+                        style={{
+                            width: "100%",
+                            backgroundColor: palette.background.default,
+                        }}
+                    />
+                </Form>
+            </Box>
+        </Modal>
     )
 
 }
