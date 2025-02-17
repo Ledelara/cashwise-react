b9f4137dd66891116eeede3963319c5398d2f81f
Leandro de Lara Celio <122060742+Ledelara@users.noreply.github.com>
2025-02-17 16:08:06 -0300

    ✨ feat: botão Sacar

--- src/app/dashboard/components/GroupedButttons/GroupedButtons.tsx
+++ src/app/dashboard/components/GroupedButttons/GroupedButtons.tsx
@@ -1,7 +1,20 @@
+import { Box } from "@mui/material";
 import DepositButton from "./components/DepositButton/DepositButton";
+import WithdrawButton from "./components/WithdrawButton/WithdrawButton";
 
 export default function GroupedButtons() {
     return (
-        <DepositButton />
+        <Box
+            sx={{
+                display: 'flex',
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                padding: '10px',
+                gap: '10px',
+            }}
+        >
+            <DepositButton />
+            <WithdrawButton />
+        </Box>
     )
 }
---
+++ src/app/dashboard/components/GroupedButttons/components/WithdrawButton/WithdrawButton.tsx
@@ -0,0 +1,25 @@
+import { AlertComponent } from "@/components/Alert/AlertComponent";
+import { AlertProvider } from "@/contexts/AlertContext/AlertContext";
+import { useState } from "react";
+import ActionButton from "../Button/Button";
+import AppIcon from "@/components/Icons/AppIcon";
+import WithdrawForm from "@/components/Forms/WithdrawForm/WithdrawForm";
+
+export default function WithdrawButton () {
+    const [isOpen, setIsOpen] = useState(false);
+
+    return (
+            <AlertProvider>
+                <ActionButton 
+                    label="Sacar"
+                    icon={<AppIcon iconTitle="Withdrawal" />}
+                    onClick={() => setIsOpen(true)}
+                />
+                <WithdrawForm 
+                    isOpen={isOpen} onClose={() => setIsOpen(false)}
+                    modalTitle="Sacar"
+                />
+                <AlertComponent />
+            </AlertProvider>
+    )
+}
