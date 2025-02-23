4982bbc1f1fae93e8f22d0efb6eff1cc2c1b077e
Leandro de Lara Celio <122060742+Ledelara@users.noreply.github.com>
2025-02-23 15:13:16 -0300

    ♻️ refactor: adicionando melhorias de usabilidade para o TransferForm

--- src/components/Forms/TransferForm/TransferForm.tsx
+++ src/components/Forms/TransferForm/TransferForm.tsx
@@ -3,106 +3,137 @@ import { useTransferAmount } from "@/services/mutate";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Box, Modal, TextField } from "@mui/material";
 import { useEffect } from "react";
-import { useForm } from "react-hook-form";
+import { Controller, useForm } from "react-hook-form";
 import Form from "../Form";
+import { z } from "zod";
+import { NumericFormat } from "react-number-format";
 
 interface TransferFormProps {
-    isOpen: boolean;
-    onClose: () => void;
-    modalTitle: string;
+  isOpen: boolean;
+  onClose: () => void;
+  modalTitle: string;
 }
 
+type FormData = z.infer<typeof transferSchema>;
+
 export default function TransferForm({ isOpen, onClose, modalTitle }: TransferFormProps) {
-    const { register, handleSubmit, formState: { errors }, reset } = useForm<{ amount: number, toAccountNumber: string }>({
-        resolver: zodResolver(transferSchema),
+    
+  const { handleSubmit, formState: { errors }, reset, control } = useForm<FormData>({
+    resolver: zodResolver(transferSchema),
+    defaultValues: {
+      amount: 0,
+      toAccountNumber: "",
+    },
+  });
+
+  const { createTransferMutation, loading } = useTransferAmount();
+  const getUserId =
+    typeof window !== "undefined" ? localStorage.getItem("userId") : null;
+
+  const onSubmit = async (data: FormData) => {
+    createTransferMutation.mutate({
+      amount: data.amount,
+      toAccountNumber: data.toAccountNumber,
+      id: getUserId ?? "",
     });
 
-    const { createTransferMutation, loading } = useTransferAmount();
-    const getUserId = typeof window !== 'undefined' ? localStorage.getItem('userId') : null;
+    if (createTransferMutation.isSuccess) {
+      onClose();
+    }
+  };
 
-    const onSubmit = async (data: { amount: number, toAccountNumber: string }) => {
-        const formattedAmount = parseFloat(String(data.amount));
-        createTransferMutation.mutate({ amount: formattedAmount, toAccountNumber: data.toAccountNumber, id: getUserId ?? '' });
-    };
+  useEffect(() => {
+    if (!isOpen) {
+      reset();
+    }
+  }, [isOpen, reset]);
 
-    useEffect(() => {
-        if (!isOpen) {
-            reset();
-        };
-    }, [isOpen, reset]);
-
-    return (
-        <Modal
-            open={isOpen}
-            onClose={onClose}
-            aria-labelledby={modalTitle}
+  return (
+    <Modal open={isOpen} onClose={onClose} aria-labelledby={modalTitle}>
+      <Box
+        sx={{
+          position: "absolute",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+          width: 300,
+          bgcolor: "background.paper",
+          borderRadius: "5px",
+          boxShadow: 24,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          padding: "10px",
+          background: "#2a5298",
+        }}
+      >
+        <Form
+          onSubmit={handleSubmit(onSubmit)}
+          style={{
+            background: "#2a5298",
+            padding: "20px",
+            borderRadius: "10px",
+            boxShadow: "0 0 10px rgba(0, 0, 0, 0.1)",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            gap: "10px",
+            width: "300px",
+          }}
+          buttonLabel={loading ? "Carregando..." : "Transferir"}
+          loading={loading}
         >
-            <Box
-                sx={{
-                    position: 'absolute',
-                    top: '50%',
-                    left: '50%',
-                    transform: 'translate(-50%, -50%)',
-                    width: 300,
-                    bgcolor: 'background.paper',
-                    borderRadius: '5px',
-                    boxShadow: 24,
-                    display: 'flex',
-                    flexDirection: 'column',
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                    padding: '10px',
-                    background: '#2a5298'
+          <Controller
+            name="amount"
+            control={control}
+            render={({ field }) => (
+              <NumericFormat
+                {...field}
+                customInput={TextField}
+                decimalSeparator=","
+                thousandSeparator="."
+                decimalScale={2}
+                fixedDecimalScale={true}
+                allowNegative={false}
+                prefix="R$ "
+                value={field.value === 0 ? "R$ 0,00" : field.value}
+                onValueChange={(values) => {
+                  field.onChange(values.floatValue);
                 }}
-            >
-                <Form
-                    onSubmit={handleSubmit(onSubmit)}
-                    style={{
-                        background: '#2a5298',
-                        padding: '20px',
-                        borderRadius: '10px',
-                        boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)',
-                        display: 'flex',
-                        flexDirection: 'column',
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                        gap: '10px',
-                        width: '300px',
-                    }}
-                    buttonLabel={loading ? 'Carregando...' : 'Transferir'}
-                    loading={loading}
-                >
-                    <TextField 
-                        id="amount"
-                        label="Valor de transferência"
-                        variant="outlined"
-                        type="text"
-                        {...register("amount", { valueAsNumber: true })}
-                        error={!!errors?.amount}
-                        helperText={errors?.amount?.message}
-                        style={{
-                            width: "100%",
-                            background: '#2a5298',
-                            color: '#fff',
-                        }}
-                    />
-                    <TextField 
-                        id="toAccountNumber"
-                        label="Número da conta de destino"
-                        variant="outlined"
-                        type="text"
-                        {...register("toAccountNumber")}
-                        error={!!errors?.toAccountNumber}
-                        helperText={errors?.toAccountNumber?.message}
-                        style={{
-                            width: "100%",
-                            background: '#2a5298',
-                            color: '#fff',
-                        }}
-                    />
-
-                </Form>
-            </Box>
-        </Modal>
-    )
+                label="Insira o valor da transferência"
+                variant="outlined"
+                error={!!errors?.amount}
+                helperText={errors?.amount?.message}
+                style={{
+                  width: "100%",
+                  background: "#2a5298",
+                  color: "#fff",
+                }}
+              />
+            )}
+          />
+          <Controller 
+            name="toAccountNumber"
+            control={control}
+            render={({ field }) => (
+              <TextField
+                {...field}
+                label="Número da conta"
+                variant="outlined"
+                error={!!errors?.toAccountNumber}
+                helperText={errors?.toAccountNumber?.message}
+                style={{
+                  width: "100%",
+                  background: "#2a5298",
+                  color: "#fff",
+                }}
+              />
+            )}
+          />
+        </Form>
+      </Box>
+    </Modal>
+  );
 }
