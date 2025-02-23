a449ac59400ba1814fc2e165e4353c133c29d901
Leandro de Lara Celio <122060742+Ledelara@users.noreply.github.com>
2025-02-23 15:01:36 -0300

    ♻️ refactor: adicionando melhorias de usabilidade para o WithdrawForm

--- src/components/Forms/WithdrawForm/WithdrawForm.tsx
+++ src/components/Forms/WithdrawForm/WithdrawForm.tsx
@@ -1,94 +1,111 @@
-import { withdrawSchema } from "@/schemas/withdrawSchema";
 import { useWithdraw } from "@/services/mutate";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Box, Modal, TextField } from "@mui/material";
 import { useEffect } from "react";
-import { useForm } from "react-hook-form";
+import { Controller, useForm } from "react-hook-form";
 import Form from "../Form";
+import { amountSchema } from "@/schemas/depositSchema";
+import { z } from "zod";
+import { NumericFormat } from "react-number-format";
 
 interface WithdrawFormProps {
-    isOpen: boolean;
-    onClose: () => void;
-    modalTitle: string;
+  isOpen: boolean;
+  onClose: () => void;
+  modalTitle: string;
 }
+type FormData = z.infer<typeof amountSchema>;
 
 export default function WithdrawForm({ isOpen, onClose, modalTitle }: WithdrawFormProps) {
 
-    const { register, handleSubmit, formState: { errors }, reset } = useForm<{ amount: number }>({
-        resolver: zodResolver(withdrawSchema),
-    });
+  const { handleSubmit, formState: { errors }, reset, control } = useForm<FormData>({
+    resolver: zodResolver(amountSchema),
+    defaultValues: { amount: 0 },
+  });
 
-    const { createWithdawMutation, loading } = useWithdraw();
-    const getUserId = typeof window !== 'undefined' ? localStorage.getItem('userId') : null;
+  const { createWithdawMutation, loading } = useWithdraw();
+  const getUserId = typeof window !== "undefined" ? localStorage.getItem("userId") : null;
 
-    const onSubmit = async (data: { amount: number }) => {
-        const formattedAmount = parseFloat(String(data.amount));
-        createWithdawMutation.mutate({ amount: formattedAmount, id: getUserId ?? '' });
-    };
+  const onSubmit = async (data: FormData) => {
+    createWithdawMutation.mutate({ amount: data.amount, id: getUserId ?? "" });
+    if (createWithdawMutation.isSuccess) {
+      onClose();
+    }
+  };
 
-    useEffect(() => {
-        if (!isOpen) {
-            reset();
-        };
-    }, [isOpen, reset]);
+  useEffect(() => {
+    if (!isOpen) {
+      reset();
+    }
+  }, [isOpen, reset]);
 
-    return (
-        <Modal
-            open={isOpen}
-            onClose={onClose}
-            aria-labelledby={modalTitle}
+  return (
+    <Modal open={isOpen} onClose={onClose} aria-labelledby={modalTitle}>
+      <Box
+        sx={{
+          position: "absolute",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+          width: 300,
+          bgcolor: "background.paper",
+          borderRadius: "5px",
+          boxShadow: 24,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          padding: "10px",
+          background: "#2a5298",
+        }}
+      >
+        <Form
+          onSubmit={handleSubmit(onSubmit)}
+          style={{
+            background: "#2a5298",
+            padding: "20px",
+            borderRadius: "10px",
+            boxShadow: "0 0 10px rgba(0, 0, 0, 0.1)",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            gap: "10px",
+            width: "300px",
+          }}
+          buttonLabel={loading ? "Carregando..." : "Realizar saque"}
+          loading={loading}
         >
-            <Box
-                sx={{
-                    position: 'absolute',
-                    top: '50%',
-                    left: '50%',
-                    transform: 'translate(-50%, -50%)',
-                    width: 300,
-                    bgcolor: 'background.paper',
-                    borderRadius: '5px',
-                    boxShadow: 24,
-                    display: 'flex',
-                    flexDirection: 'column',
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                    padding: '10px',
-                    background: '#2a5298'
+          <Controller
+            name="amount"
+            control={control}
+            render={({ field }) => (
+              <NumericFormat
+                {...field}
+                customInput={TextField}
+                decimalSeparator=","
+                thousandSeparator="."
+                decimalScale={2}
+                fixedDecimalScale={true}
+                allowNegative={false}
+                prefix="R$ "
+                value={field.value === 0 ? "R$ 0,00" : field.value}
+                onValueChange={(values) => {
+                  field.onChange(values.floatValue);
                 }}
-            >
-                <Form
-                    onSubmit={handleSubmit(onSubmit)}
-                    style={{
-                        background: '#2a5298',
-                        padding: '20px',
-                        borderRadius: '10px',
-                        boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)',
-                        display: 'flex',
-                        flexDirection: 'column',
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                        gap: '10px',
-                        width: '300px',
-                    }}
-                    buttonLabel={loading ? 'Carregando...' : 'Realizar saque'}
-                    loading={loading}
-                >
-                    <TextField 
-                        id="amount"
-                        label="Insira o valor que deseja sacar"
-                        variant="outlined"
-                        type="text"
-                        {...register("amount", { valueAsNumber: true })}
-                        error={!!errors?.amount}
-                        helperText={errors?.amount?.message}
-                        style={{
-                            width: "100%",
-                            background: '#2a5298',
-                            color: '#fff',
-                        }}
-                    />
-                </Form>
-            </Box>
-        </Modal>
-    )
+                label="Insira o valor do saque"
+                variant="outlined"
+                error={!!errors?.amount}
+                helperText={errors?.amount?.message}
+                style={{
+                  width: "100%",
+                  background: "#2a5298",
+                  color: "#fff",
+                }}
+              />
+            )}
+          />
+        </Form>
+      </Box>
+    </Modal>
+  );
 }
