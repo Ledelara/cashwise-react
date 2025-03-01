02e0591aae8c13ea1f778c73b4eae4211d969970
Leandro de Lara Celio <122060742+Ledelara@users.noreply.github.com>
2025-03-01 20:36:44 -0300

    💄 style: padronizando estilos de formulários

--- src/components/Forms/Form.tsx
+++ src/components/Forms/Form.tsx
@@ -1,18 +1,32 @@
-import { Box, Button } from "@mui/material";
+import { Box, Button, useTheme } from "@mui/material";
 import { ReactNode } from "react";
+import '@/styles/_globals.scss';
 
 interface FormProps {
     children: ReactNode;
-    style: React.CSSProperties;
     buttonLabel: string;
     loading?: boolean;
     onSubmit: (event: React.FormEvent<HTMLFormElement>) => void;
 };
 
-export default function Form({ children, style, buttonLabel, loading, onSubmit }: FormProps) {
+export default function Form({ children, buttonLabel, loading, onSubmit }: FormProps) {
+    const { palette } = useTheme();
+
     return (
         <Box
-            style={style}
+            style={{
+                backgroundColor: palette.background.paper,
+                padding: '20px',
+                borderRadius: '10px',
+                boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)',
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'center',
+                gap: '10px',
+                width: '300px',
+                background: "rgba(255, 255, 255, 0.1)",
+            }}
         >
             <form
                 onSubmit={onSubmit}
--- src/components/Forms/LoginForm/LoginForm.tsx
+++ src/components/Forms/LoginForm/LoginForm.tsx
@@ -23,19 +23,6 @@ export default function LoginForm() {
         <div>
             <Form
                 onSubmit={handleSubmit(onSubmit)}
-                style={{
-                    backgroundColor: palette.background.paper,
-                    padding: '20px',
-                    borderRadius: '10px',
-                    boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)',
-                    display: 'flex',
-                    flexDirection: 'column',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    gap: '10px',
-                    width: '300px',
-                    background: "rgba(255, 255, 255, 0.1)",
-                }}
                 buttonLabel={loading ? 'Carregando...' : 'Entrar'}
                 loading={loading}
             >
